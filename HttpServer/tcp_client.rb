5785f36c033ba3237c31f8e9383549b9a3fb692a
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-12 08:44:37 +0100

    Client app updated

--- HttpServer/tcp_client.rb
+++ HttpServer/tcp_client.rb
@@ -1,9 +1,18 @@
 # tcp_client.rb
 require 'socket'
-server = TCPSocket.new 'localhost', 5678
 
-while line = server.gets
-  puts line
-end
+host = 'blog.appsignal.com' 
+port = 80
+path = '/2016/11/23/ruby-magic-building-a-30-line-http-server-in-ruby.html'
 
-server.close
+request = "GET #{path} HTTP/1.0\r\n\r\n"
+
+socket = TCPSocket.open host, port
+socket.print request
+
+response = socket.read
+headers, body = response.split "\r\n\r\n", 2
+
+puts body
+
+socket.close
