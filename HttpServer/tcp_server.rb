edbd3ee68ebd7689785efe3c2a381e38cf57e2aa
Quynh Nguyen <qn@rosslynanalytics.com>
2017-06-12 05:47:01 +0100

    Request/response implemented

--- HttpServer/tcp_server.rb
+++ HttpServer/tcp_server.rb
@@ -1,9 +1,15 @@
-# tcp_server.rb
+# http_server.rb
 require 'socket'
 server = TCPServer.new 5678
 
 while session = server.accept
-  puts "Accepted a connection at #{Time.now}"
-  session.puts "Hello world! The time is #{Time.now}"
+  request = session.gets
+  puts request
+
+  session.print "HTTP/1.1 200\r\n" # 1
+  session.print "Content-Type: text/html\r\n" # 2
+  session.print "\r\n" # 3
+  session.print "Hello world! The time is #{Time.now}" #4
+
   session.close
 end
